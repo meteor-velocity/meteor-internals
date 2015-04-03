baaba15e365069c97490fb8a5d1795bc2842e3b4
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-04-04 00:15:37 +0200

    Updates for Meteor 1.1 release and adds cross os files functions

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'velocity:meteor-internals',
   summary: 'Exposes internals of Meteor that are useful for Velocity',
-  version: '1.0.2_2',
+  version: '1.1.0_1',
   git: 'https://github.com/meteor-velocity/meteor-internals.git'
 });
 
@@ -13,12 +13,25 @@ Package.onUse(function(api) {
     'main.js',
     'tools/parse-stack.js',
     'tools/buildmessage.js',
-    'tools/files.js'
+    'tools/files.js',
+    'tools/server/mini-files.js'
   ], 'server');
 });
+
+var fibersVersion;
+if (process.platform === "win32") {
+  // We have a fork of fibers off of version 1.0.5 that searches farther for
+  // the isolate thread. This problem is a result of antivirus programs messing
+  // with the thread counts on Windows.
+  // Duplicated in dev-bundle-tool-package.js
+  fibersVersion = "https://github.com/meteor/node-fibers/tarball/d519f0c5971c33d99c902dad346b817e84bab001";
+} else {
+  fibersVersion = "1.0.5";
+}
 
 Npm.depends({
   'source-map': '0.1.40',
   'source-map-support': '0.2.8',
-  'esprima': '1.2.2'
+  'esprima': '1.2.2',
+  'fibers': fibersVersion
 });
--- tools/files.js
+++ tools/files.js
@@ -14,6 +14,7 @@
 
 var fs = Npm.require("fs");
 var path = Npm.require('path');
+var Fiber = require('fibers');
 var sourcemap = Npm.require('source-map');
 var sourcemap_support = Npm.require('source-map-support');
 
@@ -45,12 +46,12 @@ sourcemap_support.install({
 // the test or null for none found. if starting path isn't given, use
 // cwd.
 var findUpwards = function (predicate, startPath) {
-  var testDir = startPath || process.cwd();
+  var testDir = startPath || files.cwd();
   while (testDir) {
     if (predicate(testDir)) {
       break;
     }
-    var newDir = path.dirname(testDir);
+    var newDir = files.pathDirname(testDir);
     if (newDir === testDir) {
       testDir = null;
     } else {
@@ -63,20 +64,25 @@ var findUpwards = function (predicate, startPath) {
   return testDir;
 };
 
+files.cwd = function () {
+  return files.convertToStandardPath(process.cwd());
+};
+
 // Determine if 'filepath' (a path, or omit for cwd) is within an app
 // directory. If so, return the top-level app directory.
 files.findAppDir = function (filepath) {
   var isAppDir = function (filepath) {
     // XXX once we are done with the transition to engine, this should
-    // change to: `return fs.existsSync(path.join(filepath, '.meteor',
+    // change to: `return files.exists(path.join(filepath, '.meteor',
     // 'release'))`
 
     // .meteor/packages can be a directory, if .meteor is a warehouse
     // directory.  since installing meteor initializes a warehouse at
     // $HOME/.meteor, we want to make sure your home directory (and all
     // subdirectories therein) don't count as being within a meteor app.
-    try { // use try/catch to avoid the additional syscall to fs.existsSync
-      return fs.statSync(path.join(filepath, '.meteor', 'packages')).isFile();
+    try { // use try/catch to avoid the additional syscall to files.exists
+      return files.stat(
+        files.pathJoin(filepath, '.meteor', 'packages')).isFile();
     } catch (e) {
       return false;
     }
@@ -88,7 +94,7 @@ files.findAppDir = function (filepath) {
 files.findPackageDir = function (filepath) {
   var isPackageDir = function (filepath) {
     try {
-      return fs.statSync(path.join(filepath, 'package.js')).isFile();
+      return files.stat(files.pathJoin(filepath, 'package.js')).isFile();
     } catch (e) {
       return false;
     }
@@ -160,7 +166,7 @@ files.runJavaScript = function (code, options) {
     parsedSourceMap = results.map.toJSON();
     if (options.sourceMapRoot) {
       // Add the specified root to any root that may be in the file.
-      parsedSourceMap.sourceRoot = path.join(
+      parsedSourceMap.sourceRoot = files.pathJoin(
         options.sourceMapRoot, parsedSourceMap.sourceRoot || '');
     }
     // source-map-support doesn't ever look at the sourcesContent field, so
@@ -252,3 +258,197 @@ files.runJavaScript = function (code, options) {
 // - line: 1-based
 // - column: 1-based
 files.FancySyntaxError = function () {};
+
+// Summary of cross platform file system handling strategy
+
+// There are three main pain points for handling files on Windows: slashes in
+// paths, line endings in text files, and colons/invalid characters in paths.
+
+// 1. Slashes in file paths
+
+//   We have decided to store all paths inside the tool as unix-style paths in
+//   the style of CYGWIN. This means that all paths have forward slashes on all
+//   platforms, and C:\ is converted to /c/ on Windows.
+
+//   All of the methods in files.js know how to convert from these unixy paths
+//   to whatever type of path the underlying system prefers.
+
+//   The reason we chose this strategy because it was easier to make sure to use
+//   files.js everywhere instead of node's fs than to make sure every part of
+//   the tool correctly uses system-specific path separators. In addition, there
+//   are some parts of the tool where it is very hard to tell which strings are
+//   used as URLs and which are used as file paths. In some cases, a string can
+//   be used as both, meaning it has to have forward slashes no matter what.
+
+// 2. Line endings in text files
+
+//   We have decided to convert all files read by the tool to Unix-style line
+//   endings for the same reasons as slashes above. In many parts of the tool,
+//   we assume that '\n' is the line separator, and it can be hard to find all
+//   of the places and decide whether it is appropriate to use os.EOL. We do not
+//   convert anything on write. We will wait and see if anyone complains.
+
+// 3. Colons and other invalid characters in file paths
+
+//   This is not handled automatically by files.js. You need to be careful to
+//   escape any colons in package names, etc, before using a string as a file
+//   path.
+
+//   A helpful file to import for this purpose is colon-converter.js, which also
+//   knows how to convert various configuration file formats.
+
+/**
+ * Wrap a function from node's fs module to use the right slashes for this OS
+ * and run in a fiber, then assign it to the "files" namespace. Each call
+ * creates a files.func that runs asynchronously with Fibers (yielding and
+ * until the call is done), unless run outside a Fiber or in noYieldsAllowed, in
+ * which case it uses fs.funcSync.
+ *
+ * @param  {String} fsFuncName         The name of the node fs function to wrap
+ * @param  {Number[]} pathArgIndices Indices of arguments that have paths, these
+ * arguments will be converted to the correct OS slashes
+ * @param  {Object} options        Some options for lesser-used cases
+ * @param {Boolean} options.noErr If true, the callback of the wrapped function
+ * doesn't have a first "error" argument, for example in fs.exists.
+ * @param {Function} options.modifyReturnValue Pass in a function to modify the
+ * return value
+ */
+function wrapFsFunc(fsFuncName, pathArgIndices, options) {
+  options = options || {};
+
+  var fsFunc = fs[fsFuncName];
+  var fsFuncSync = fs[fsFuncName + "Sync"];
+
+  function wrapper() {
+    var argc = arguments.length;
+    var args = new Array(argc);
+    for (var i = 0; i < argc; ++i) {
+      args[i] = arguments[i];
+    }
+
+    for (var j = pathArgIndices.length - 1; j >= 0; --j) {
+      i = pathArgIndices[j];
+      args[i] = files.convertToOSPath(args[i]);
+    }
+
+    if (Fiber.current &&
+      Fiber.yield && ! Fiber.yield.disallowed) {
+      var fut = new Future;
+
+      args.push(function callback(err, value) {
+        if (options.noErr) {
+          fut.return(err);
+        } else if (err) {
+          fut.throw(err);
+        } else {
+          fut.return(value);
+        }
+      });
+
+      fsFunc.apply(fs, args);
+
+      var result = fut.wait();
+      return options.modifyReturnValue
+        ? options.modifyReturnValue(result)
+        : result;
+    }
+
+    // If we're not in a Fiber, run the sync version of the fs.* method.
+    var result = fsFuncSync.apply(fs, args);
+    return options.modifyReturnValue
+      ? options.modifyReturnValue(result)
+      : result;
+  }
+
+  wrapper.displayName = fsFuncName;
+  return files[fsFuncName] = Profile("files." + fsFuncName, wrapper);
+}
+
+wrapFsFunc("writeFile", [0]);
+wrapFsFunc("appendFile", [0]);
+wrapFsFunc("readFile", [0], {
+  modifyReturnValue: function (fileData) {
+    if (_.isString(fileData)) {
+      return files.convertToStandardLineEndings(fileData);
+    }
+
+    return fileData;
+  }
+});
+wrapFsFunc("stat", [0]);
+wrapFsFunc("lstat", [0]);
+wrapFsFunc("exists", [0], {noErr: true});
+wrapFsFunc("rename", [0, 1]);
+
+if (process.platform === "win32") {
+  var rename = files.rename;
+
+  files.rename = function (from, to) {
+    // retries are necessarily only on Windows, because the rename call can fail
+    // with EBUSY, which means the file is "busy"
+    var maxTries = 10;
+    var success = false;
+    while (! success && maxTries-- > 0) {
+      try {
+        rename(from, to);
+        success = true;
+      } catch (err) {
+        if (err.code !== 'EPERM')
+          throw err;
+      }
+    }
+    if (! success) {
+      files.cp_r(from, to);
+      files.rm_recursive(from);
+    }
+  };
+}
+
+// Warning: doesn't convert slashes in the second 'cache' arg
+wrapFsFunc("realpath", [0], {
+  modifyReturnValue: files.convertToStandardPath
+});
+
+wrapFsFunc("readdir", [0], {
+  modifyReturnValue: function (entries) {
+    return _.map(entries, files.convertToStandardPath);
+  }
+});
+
+wrapFsFunc("rmdir", [0]);
+wrapFsFunc("mkdir", [0]);
+wrapFsFunc("unlink", [0]);
+wrapFsFunc("chmod", [0]);
+wrapFsFunc("open", [0]);
+
+// XXX this doesn't give you the second argument to the callback
+wrapFsFunc("read", []);
+wrapFsFunc("write", []);
+wrapFsFunc("close", []);
+wrapFsFunc("symlink", [0, 1]);
+wrapFsFunc("readlink", [0]);
+
+// These don't need to be Fiberized
+files.createReadStream = function () {
+  var args = _.toArray(arguments);
+  args[0] = files.convertToOSPath(args[0]);
+  return fs.createReadStream.apply(fs, args);
+};
+
+files.createWriteStream = function () {
+  var args = _.toArray(arguments);
+  args[0] = files.convertToOSPath(args[0]);
+  return fs.createWriteStream.apply(fs, args);
+};
+
+files.watchFile = function () {
+  var args = _.toArray(arguments);
+  args[0] = files.convertToOSPath(args[0]);
+  return fs.watchFile.apply(fs, args);
+};
+
+files.unwatchFile = function () {
+  var args = _.toArray(arguments);
+  args[0] = files.convertToOSPath(args[0]);
+  return fs.unwatchFile.apply(fs, args);
+};

f6e5d2c803551c347841f3669d4923f330c48a77
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-04-04 00:27:47 +0200

    Removes dependency to fibers node module

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'velocity:meteor-internals',
   summary: 'Exposes internals of Meteor that are useful for Velocity',
-  version: '1.1.0_1',
+  version: '1.1.0_2',
   git: 'https://github.com/meteor-velocity/meteor-internals.git'
 });
 
@@ -18,20 +18,8 @@ Package.onUse(function(api) {
   ], 'server');
 });
 
-var fibersVersion;
-if (process.platform === "win32") {
-  // We have a fork of fibers off of version 1.0.5 that searches farther for
-  // the isolate thread. This problem is a result of antivirus programs messing
-  // with the thread counts on Windows.
-  // Duplicated in dev-bundle-tool-package.js
-  fibersVersion = "https://github.com/meteor/node-fibers/tarball/d519f0c5971c33d99c902dad346b817e84bab001";
-} else {
-  fibersVersion = "1.0.5";
-}
-
 Npm.depends({
   'source-map': '0.1.40',
   'source-map-support': '0.2.8',
-  'esprima': '1.2.2',
-  'fibers': fibersVersion
+  'esprima': '1.2.2'
 });
